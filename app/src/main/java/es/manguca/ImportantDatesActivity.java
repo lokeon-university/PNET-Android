cb396b47f17755ab81e11d9f95d7ee68d278e8a9
Lokeon <kevin.lopezcala99@gmail.com>
2020-05-21 18:23:56 +0200

    :penguin: added POST PUT DELETE method

--- app/src/main/java/es/manguca/ImportantDatesActivity.java
+++ app/src/main/java/es/manguca/ImportantDatesActivity.java
@@ -19,9 +19,12 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -89,6 +92,9 @@ public class ImportantDatesActivity extends AppCompatActivity {
 
 
         new GetAssistant().execute();
+        //new PostAssistant().execute();
+        //new PutAssistant().execute();
+        //new DeleteAssistant().execute();
     }
 
     class GetAssistant extends AsyncTask<Void,Void,String>{
@@ -139,4 +145,145 @@ public class ImportantDatesActivity extends AppCompatActivity {
         }
     }
 
+    class PostAssistant extends AsyncTask<Void,Void,String>{
+        @Override
+        protected String doInBackground(Void... strings) {
+            String text = null;
+            BufferedWriter bufferedWriter = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                JSONObject dataToSend = new JSONObject();
+
+                dataToSend.put("name", "Pepe");
+                dataToSend.put("surname", "Manolo");
+                dataToSend.put("email", 1);
+                dataToSend.put("telephone", 1);
+                dataToSend.put("dni",1231231);
+                dataToSend.put("birthday",1111);
+                dataToSend.put("date_insription",1231);
+
+                URL url = new URL(getResources().getString(R.string.ip_node));
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                if (urlConnection.getResponseCode() == 200) {
+                    text = "Post successfully !";
+                } else {
+                    text =  "Post failed !";
+                }
+
+            } catch (Exception e ) {
+                return e.toString();
+            } finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String results) {
+            super.onPostExecute(results);
+            // aqui meter cosa de que ha enviado los datos
+        }
+    }
+
+    class PutAssistant extends AsyncTask<Void,Void,String> {
+
+        @Override
+        protected String doInBackground(Void... stings) {
+            String text = null;
+            BufferedWriter bufferedWriter = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                JSONObject dataToSend = new JSONObject();
+
+                dataToSend.put("name", "Pepe111111111111");
+                dataToSend.put("surname", "Manolo2asdasda");
+                dataToSend.put("email", "1@email.com");
+                dataToSend.put("telephone", 1);
+                dataToSend.put("dni",1231231);
+                dataToSend.put("birthday",1111);
+                dataToSend.put("date_insription",1231);
+
+                URL url = new URL(getResources().getString(R.string.ip_node) + "/5ec6a2f099915247707e6e6f");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataToSend.toString());
+                bufferedWriter.flush();
+
+                if (urlConnection.getResponseCode() == 200) {
+                    text = "Update successfully !";
+                } else {
+                    text =  "Update failed !";
+                }
+
+            } catch (Exception e ) {
+                return e.toString();
+            } finally {
+                if(urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            // aqui meter mensaje de que se ha creado
+        }
+    }
+
+    class DeleteAssistant extends AsyncTask<Void,Void,String> {
+
+        @Override
+        protected String doInBackground(Void... strings) {
+            String text = null;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                URL url = new URL(getResources().getString(R.string.ip_node) + "/5ec65019b5e1930c6d10d3e0");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("DELETE");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                if (urlConnection.getResponseCode() == 200) {
+                    text = "Delete successfully !";
+                } else {
+                    text = "Delete failed !";
+                }
+
+            } catch (Exception e) {
+                return e.toString();
+            } finally {
+                if (urlConnection != null)
+                    urlConnection.disconnect();
+            }
+            return text;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            // aqui meter mensaje de lo que sea
+        }
+    }
 }
