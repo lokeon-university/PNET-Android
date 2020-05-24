70d4a339857229345a9c06a05414c3216dce3ab8
Lokeon <kevin.lopezcala99@gmail.com>
2020-05-24 19:57:26 +0200

    :penguin: notifications working

--- app/src/main/java/es/manguca/ImportantDatesActivity.java
+++ app/src/main/java/es/manguca/ImportantDatesActivity.java
@@ -2,10 +2,20 @@ package es.manguca;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RemoteViews;
 
 
 public class ImportantDatesActivity extends AppCompatActivity {
@@ -31,6 +41,7 @@ public class ImportantDatesActivity extends AppCompatActivity {
         Button btn_assistant = (Button)findViewById(R.id.button_assistant);
         Button btn_location = (Button)findViewById(R.id.button_location);
         Button btn_date = (Button)findViewById(R.id.button_date);
+        Button btn_notification = (Button) findViewById(R.id.not);
 
         btn_home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,5 +78,54 @@ public class ImportantDatesActivity extends AppCompatActivity {
             }
         });
 
+
+        btn_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotification("aaaaaaa","eeeeeee");
+            }
+        });
+    }
+
+    private RemoteViews getCustomDesign(String title, String message) {
+        RemoteViews remoteViews = new RemoteViews(getApplicationContext().getPackageName(), R.layout.notification);
+        remoteViews.setTextViewText(R.id.title, title);
+        remoteViews.setTextViewText(R.id.message, message);
+        remoteViews.setImageViewResource(R.id.icon, R.drawable.logo_gato);
+        return remoteViews;
+    }
+
+    public void showNotification(String title,String message) {
+        Intent intent = new Intent(this, LocationActivity.class);
+        String channel_id = "manuguca_location_channel";
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),channel_id)
+                .setSmallIcon(R.drawable.logo_gato)
+                .setSound(uri)
+                .setAutoCancel(true)
+                .setVibrate(new long[]{1000,1000,1000,1000,1000})
+                .setOnlyAlertOnce(true)
+                .setContentIntent(pendingIntent);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            builder = builder.setContent(getCustomDesign(title,message));
+        }
+        else {
+            builder = builder.setContentTitle(title)
+                    .setContentText(message)
+                    .setSmallIcon(R.drawable.logo_gato);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(channel_id,"manguca_location",NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setSound(uri,null);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        notificationManager.notify(0,builder.build());
     }
 }
